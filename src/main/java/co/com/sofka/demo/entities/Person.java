124866bddf97a696bee98eb424ff37cbcdb1995f
Cristian Cabarcas <cridejecato@gmail.com>
2021-12-29 00:12:14 -0500

    First commit

---
+++ src/main/java/co/com/sofka/demo/entities/Person.java
@@ -0,0 +1,30 @@
+package co.com.sofka.demo.entities;
+
+public class Person {
+
+    private String name;
+    private String age;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
